feaacb8c88748edabe2acba5ac082614699d7326
thegirldweez <damerisbaker@outlook.com>
2024-01-06 19:11:06 -0800

    complete node.js practice lessons 1 - 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function () {
+  let text = "hey";
+  let result = text.repeat(3);
 
+  return result;
 };
+
+console.log(repeatString());
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,29 @@
-const reverseString = function() {
+const reverseString = function (parameter) {
+  const greeting = parameter;
 
+  let greeting1 = greeting.split("");
+
+  let reversedGreeting = greeting1.reverse();
+
+  return reversedGreeting.join("");
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc!");
+reverseString("");
+
+/*
+const reverseString = function () {
+  const greetings = "hello there";
+
+  let greetingTwo = greetings.split("");
+
+  let reversedGreetings = greetingTwo.reverse();
+
+  return reversedGreetings.join("");
+};
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
